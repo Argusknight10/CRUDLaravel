5594f745481d3c3036bb9a88ec614108a386c439
argus <arguspermono19@gmail.com>
2024-10-15 03:16:35 +0700

    update .env agar menggunakan mysql

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -15,21 +15,24 @@ use Illuminate\Support\Facades\Storage;
 
 class BeritaController extends Controller
 {
-    public function index() : View{
+    public function index(): View
+    {
         Gate::authorize('admin');
 
         $beritas = Berita::latest()->paginate(10);
         // return view('beritas.index', compact('beritas')); // Sama Saja
 
-        return view('beritas.index', ['title' => 'NEWS PAGE', 'beritas' => $beritas]) ;
+        return view('beritas.index', ['title' => 'NEWS PAGE', 'beritas' => $beritas]);
     }
 
-    public function create() : View{
+    public function create(): View
+    {
         Gate::authorize('admin');
         return view('beritas.create', ['title' => 'ADD NEWS', 'kategori' => Kategori::all()]);
     }
 
-    public function store(Request $request): RedirectResponse{
+    public function store(Request $request): RedirectResponse
+    {
         $request->validate([
             'title'         => 'required|unique:beritas',
             'image'         => 'required|image|mimes:jpeg,jpg,png|max:500000000',
@@ -48,28 +51,31 @@ class BeritaController extends Controller
             'title'         => $request->title,
             'slug'          => $slug,
             'image'         => $filename,
-            'kategori_id'      => $request->kategori,
-            'deskripsi'   => $request->deskripsi,
+            'kategori_id'   => $request->kategori,
+            'deskripsi'     => $request->deskripsi,
         ]);
-        
+
         return redirect()->route('beritas.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 
-    public function show(string $id) : View {
+    public function show(string $id): View
+    {
         $beritas = Berita::findOrFail($id);
         $beritaLain = Berita::paginate(10);
 
         return view('beritas.show', ['title' => 'DETAIL NEWS', 'beritas' => $beritas, 'beritaLain' => $beritaLain]);
     }
 
-    public function edit(string $id) : View {
+    public function edit(string $id): View
+    {
         Gate::authorize('admin');
-        $beritas = Berita:: findOrFail($id);
+        $beritas = Berita::findOrFail($id);
 
         return view('beritas.edit', ['title' => 'EDIT NEWS', 'beritas' => $beritas, 'kategori' => Kategori::all()]);
     }
 
-    public function update(Request $request, $id): RedirectResponse{
+    public function update(Request $request, $id): RedirectResponse
+    {
         $beritas = Berita::findOrFail($id);
 
         $request->validate([
@@ -83,8 +89,8 @@ class BeritaController extends Controller
         ]);
 
         if ($request->hasFile('image')) {
-            if ($beritas->image) { 
-                Storage::disk('public')->delete('img/' . $beritas->image); 
+            if ($beritas->image) {
+                Storage::disk('public')->delete('img/' . $beritas->image);
             }
 
             $image = $request->file('image');
@@ -96,7 +102,7 @@ class BeritaController extends Controller
 
         $kategoriId = $request->kategori;
         if ($kategoriId === null) {
-            $kategoriId = $beritas->kategori_id; 
+            $kategoriId = $beritas->kategori_id;
         }
 
         $slug = Str::slug($request->title);
@@ -112,11 +118,12 @@ class BeritaController extends Controller
         return redirect()->route('beritas.index')->with(['success' => 'Perubahan Berhasil Disimpan!']);
     }
 
-    public function destroy($id): RedirectResponse{
+    public function destroy($id): RedirectResponse
+    {
         Gate::authorize('admin');
         $beritas = Berita::findOrFail($id);
 
-        if ($beritas->image) { 
+        if ($beritas->image) {
             Storage::disk('public')->delete('img/' . $beritas->image);
         }
         $beritas->delete();
